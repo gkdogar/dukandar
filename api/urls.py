7ed9ae3b24432dbc78e8ef644d20536fe36701e3
gkdogar <gk.dogar@gmail.com>
2022-02-11 09:20:43 +0500

    merge with work from home

--- api/urls.py
+++ api/urls.py
@@ -17,7 +17,6 @@ router.register('customer', views.CustomerViewSetApi,basename='customers')
 router.register('products', views.ProductViewSetApi,basename='products')
 router.register('orders', views.OrderViewSetApi,basename='orders')
 router.register('wallet', views.WalletViewSetApi,basename='wallet')
-
 router.register('spines', views.SpinesViewSetApi,basename='spines')
 
 app_name = 'api'
